9251a3316ca02b124ffd838bf434304e6e01affc
Dagim Tesfaye Mebrate <dagimcodes@gmail.com>
2024-06-24 00:36:42 +0300

    feat: splash screen and icon

--- app/src/main/java/tech/dagimtesfaye/cbe_balance_tracker/MainActivity.kt
+++ app/src/main/java/tech/dagimtesfaye/cbe_balance_tracker/MainActivity.kt
@@ -10,11 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import tech.dagimtesfaye.cbe_balance_tracker.ui.theme.CBEBalanceTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             CBEBalanceTrackerTheme {
                 // A surface container using the 'background' color from the theme
